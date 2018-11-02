6e711e43f84ea4c948f8d02199b5b2f8c07d2a2d
Andrii Dimov <andimov@gmail.com>
2018-11-03 01:16:34 +0200

    Update magentoSettings.sh

--- magentoSettings.sh
+++ magentoSettings.sh
@@ -1,5 +1,4 @@
-php bin/magento cache:flush && 
-php bin/magento indexer:set-mode schedule &&
+php bin/magento indexer:set-mode schedule
 rm -rf pub/static/* var/view_preprocessed/pub
 php -f bin/magento setup:store-config:set --admin-use-security-key=0 --use-rewrites=1
 php bin/magento config:set --scope default -- admin/security/session_lifetime 7200
@@ -11,4 +10,5 @@ php bin/magento config:set --scope default -- dev/js/enable_js_bundling 1
 php bin/magento config:set --scope default -- dev/js/minify_files 1
 php bin/magento config:set --scope default -- dev/css/merge_css_files 1
 php bin/magento config:set --scope default -- dev/css/minify_files 1
-php bin/magento setup:static-content:deploy 
+php bin/magento setup:static-content:deploy
+php bin/magento cache:flush
