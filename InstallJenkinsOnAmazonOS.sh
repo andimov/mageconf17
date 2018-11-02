f0f49830bbd31898653cc7f8a9b399dc42b4baf8
Andrii Dimov <andimov@gmail.com>
2018-11-03 01:45:20 +0200

    Update InstallJenkinsOnAmazonOS.sh

--- InstallJenkinsOnAmazonOS.sh
+++ InstallJenkinsOnAmazonOS.sh
@@ -2,7 +2,7 @@
 yum update –y
 yum install java -y
 yum install -y docker
-service docker start
+systemctl start docker
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install jenkins -y
